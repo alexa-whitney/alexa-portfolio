ed3476934a5c47cb3dbd452c3f8e83fa70cce5ac
Alexa Whitney <alexa.whitney@shopify.com>
2023-04-26 12:13:17 -0600

    Added fonts, images, and navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { NavBar, Container } from "react-bootstrap";
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+    <Container>
+      <Navbar.Brand href="#home">
+        <img src={''} alt="Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <span className="navbar-toggler-icon"></span>
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+          <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+          <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+        </Nav>
+        <span className="navbar-text">
+          <div className="social-icons">
+            <a href="#"><img src={''} alt="" /></a>
+            <a href="#"><img src={''} alt="" /></a>
+            <a href="#"><img src={''} alt="" /></a>
+          </div>
+          <button ClassName="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+        </span>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
+}
