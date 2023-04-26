d441a3824b932e8764ae12deabb04ac6d6917c45
Alexa Whitney <alexa.whitney@shopify.com>
2023-04-26 12:56:30 -0600

    NavBar rendering successfully

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { MyNavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <MyNavBar />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
-import { NavBar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '../assets/images/logo.png';
+import navIcon1 from '../assets/images/nav-icon1.svg';
+import navIcon2 from '../assets/images/nav-icon2.svg';
+import navIcon3 from '../assets/images/nav-icon3.svg';
 
-export const NavBar = () => {
+export const MyNavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
@@ -27,7 +31,7 @@ export const NavBar = () => {
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
     <Container>
       <Navbar.Brand href="#home">
-        <img src={''} alt="Logo" />
+        <img src={logo} alt="Logo" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav">
         <span className="navbar-toggler-icon"></span>
@@ -40,9 +44,9 @@ export const NavBar = () => {
         </Nav>
         <span className="navbar-text">
           <div className="social-icons">
-            <a href="#"><img src={''} alt="" /></a>
-            <a href="#"><img src={''} alt="" /></a>
-            <a href="#"><img src={''} alt="" /></a>
+            <a href="#"><img src={navIcon1} alt="" /></a>
+            <a href="#"><img src={navIcon2} alt="" /></a>
+            <a href="#"><img src={navIcon3} alt="" /></a>
           </div>
           <button ClassName="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
         </span>
