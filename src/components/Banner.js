8b093e4d179f72c52fe6df66959ed21e698e5d0d
alexa-whitney <alexawhitney@gmail.com>
2023-11-06 11:23:41 -0700

    Updated into text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -71,7 +71,7 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hi I'm Alexa `}
+              {`Hi, I'm Alexa! I am a `}
               <span className="wrap">{text}</span>
             </h1>
             <p>
